2f1a8e973596c1fdfd3b268f64ef3c73a1061784
Kutlu Ata <32374530+leto666@users.noreply.github.com>
2019-12-23 20:22:35 +0300

    Add files via upload

---
+++ precache-manifest.9880f11aa55c58ac74b0980c46d68078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef96a9d91118be501d1d6c82c5cc1146",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08ab3cc921029aba8f78",
+    "url": "./static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d785987eeca10779a67d",
+    "url": "./static/css/main.0a3a1f41.chunk.css"
+  },
+  {
+    "revision": "08ab3cc921029aba8f78",
+    "url": "./static/js/2.9d4447c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.9d4447c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d785987eeca10779a67d",
+    "url": "./static/js/main.7b764cff.chunk.js"
+  },
+  {
+    "revision": "a0185f74eac5e9c319ec",
+    "url": "./static/js/runtime-main.b2a781e9.js"
+  }
+]);
